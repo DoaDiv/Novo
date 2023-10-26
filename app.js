4aa24da66f10ef828d12c3a38cc34fe440f122f2
DoaDiv <134438535+DoaDiv@users.noreply.github.com>
2023-10-26 20:17:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
 const app = express();
 const port = 8081;
 
@@ -7,6 +9,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+let volunteerCount = 0; 
+
+app.post('/', (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, fields) => {
+    if (err) {
+      res.status(500).send('Erro interno do servidor, volte a página ou entre novamente no site.');
+      return;
+    }
+
+    const data = `Nome: ${fields.nome}\nTelefone: ${fields.telefone}\nEmail: ${fields.email}\nData de Nascimento: ${fields.nascimento}\nDisponibilidade: ${fields.disponibilidade}\n\n`;
+
+    const fileName = `voluntario${++volunteerCount}.txt`;
+
+    const filePath = path.join(__dirname, 'Voluntario', fileName);
+
+    fs.writeFile(filePath, data, (err) => {
+      if (!err) {
+ 
+        res.redirect('/');
+      } else {
+        res.status(500).send('Erro interno do servidor, volte a página ou entre novamente no site.');
+      }
+    });
+  });
 });
 
 app.listen(port, () => {
